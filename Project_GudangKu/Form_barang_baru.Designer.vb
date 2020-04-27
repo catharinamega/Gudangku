a7f53d3fc78ef520ea2e27d604856889c44668bd
kristi4n <kristianlie6@gmail.com>
2020-04-27 22:54:13 +0800

    rr

---
+++ Project_GudangKu/Form_barang_baru.Designer.vb
@@ -0,0 +1,93 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form_barang_baru
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.panel_menu = New System.Windows.Forms.Panel()
+        Me.btn_min = New System.Windows.Forms.Button()
+        Me.btn_max = New System.Windows.Forms.Button()
+        Me.btn_close = New System.Windows.Forms.Button()
+        Me.panel_menu.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'panel_menu
+        '
+        Me.panel_menu.BackColor = System.Drawing.Color.White
+        Me.panel_menu.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.panel_menu.Controls.Add(Me.btn_min)
+        Me.panel_menu.Controls.Add(Me.btn_max)
+        Me.panel_menu.Controls.Add(Me.btn_close)
+        Me.panel_menu.Location = New System.Drawing.Point(0, 0)
+        Me.panel_menu.Name = "panel_menu"
+        Me.panel_menu.Size = New System.Drawing.Size(800, 30)
+        Me.panel_menu.TabIndex = 2
+        '
+        'btn_min
+        '
+        Me.btn_min.BackColor = System.Drawing.Color.Transparent
+        Me.btn_min.Location = New System.Drawing.Point(711, 2)
+        Me.btn_min.Name = "btn_min"
+        Me.btn_min.Size = New System.Drawing.Size(24, 23)
+        Me.btn_min.TabIndex = 2
+        Me.btn_min.Text = "-"
+        Me.btn_min.UseVisualStyleBackColor = False
+        '
+        'btn_max
+        '
+        Me.btn_max.BackColor = System.Drawing.Color.Transparent
+        Me.btn_max.Location = New System.Drawing.Point(741, 2)
+        Me.btn_max.Name = "btn_max"
+        Me.btn_max.Size = New System.Drawing.Size(24, 23)
+        Me.btn_max.TabIndex = 1
+        Me.btn_max.Text = "[:]"
+        Me.btn_max.UseVisualStyleBackColor = False
+        '
+        'btn_close
+        '
+        Me.btn_close.BackColor = System.Drawing.Color.Transparent
+        Me.btn_close.Location = New System.Drawing.Point(771, 2)
+        Me.btn_close.Name = "btn_close"
+        Me.btn_close.Size = New System.Drawing.Size(24, 23)
+        Me.btn_close.TabIndex = 0
+        Me.btn_close.Text = "X"
+        Me.btn_close.UseVisualStyleBackColor = False
+        '
+        'Form_barang_baru
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.panel_menu)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form_barang_baru"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form_barang_baru"
+        Me.panel_menu.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents panel_menu As Panel
+    Friend WithEvents btn_min As Button
+    Friend WithEvents btn_max As Button
+    Friend WithEvents btn_close As Button
+End Class
---
+++ Project_GudangKu/Form_barang_baru.vb
@@ -0,0 +1,3 @@
+﻿Public Class Form_barang_baru
+
+End Class
